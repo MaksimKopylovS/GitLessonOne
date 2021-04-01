46d1a193dbb6708a2c2375d5169deb4a52770b37
Maksim <maksim.kopylov.x5@gmail.com>
2021-04-01 16:20:13 +0300

    commithotfix

--- src/main/java/max_sk/SpringApplication.java
+++ src/main/java/max_sk/SpringApplication.java
@@ -3,5 +3,6 @@ package max_sk;
 public class SpringApplication {
     public static void main(String[] args) {
         System.out.println("Мой проект для Гита");
+        System.out.println("HotFix");
     }
 }
