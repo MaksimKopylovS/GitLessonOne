7ba82dda5023b2da24aed72df1629717ab6bd10d
Maksim <maksim.kopylov.x5@gmail.com>
2021-04-02 11:00:36 +0300

    spring commit One

--- src/main/java/max_sk/SpringApplication.java
+++ src/main/java/max_sk/SpringApplication.java
@@ -8,5 +8,6 @@ public class SpringApplication {
         System.out.println("Zakonchil modifikaciu v Spring");
         System.out.println("Rabora v master verke Commit ONe");
         System.out.println("Rabora v master verke Commit Two");
+        System.out.println("Rabora v Spring vetke Commit One");
     }
 }
